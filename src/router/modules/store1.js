7af13f7b3671690b12fddf7c9bbb84f669d65d61
Roy <rxlisbest@gmail.com>
2019-11-14 16:29:09 +0800

    路由修改

--- src/router/modules/store1.js
+++ src/router/modules/store1.js
@@ -8,27 +8,27 @@ export default {
       path: 'module_id/1',
       name: '专业市场',
       component: () => import('@/views/stores/index'),
-      meta: { title: '专业市场', icon: 'list', noCache: true, parent_module_id: 0, module_id: 1, roles: [1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] },
+      meta: { title: '专业市场', icon: 'list', noCache: true, parent_module_id: 0, module_id: 1, roles: [1] },
     },
     {
       path: 'module_id/:module_id(\\d+)/create',
       name: '新建',
       component: () => import('@/views/stores/create'),
-      meta: { title: '新建', noCache: true, parent_module_id: 0, module_id: 1, activeMenu: '/stores/0/module_id/1', roles: [1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] },
+      meta: { title: '新建', noCache: true, parent_module_id: 0, module_id: 1, activeMenu: '/stores/0/module_id/1', roles: [1] },
       hidden: true
     },
     {
       path: 'module_id/:module_id(\\d+)/edit/:id(\\d+)',
       component: () => import('@/views/stores/edit'),
       name: '编辑',
-      meta: { title: '编辑', noCache: true, parent_module_id: 0, module_id: 1, activeMenu: '/stores/0/module_id/1', roles: [1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] },
+      meta: { title: '编辑', noCache: true, parent_module_id: 0, module_id: 1, activeMenu: '/stores/0/module_id/1', roles: [1] },
       hidden: true
     },
     {
       path: 'module_id/:parent_id(\\d+)/children/:id(\\d+)',
       name: '下级列表',
       component: () => import('@/views/stores/children'),
-      meta: { title: '下级列表', icon: 'list', noCache: true, parent_module_id: 0, module_id: 1, activeMenu: '/stores/0/module_id/1', roles: [1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] },
+      meta: { title: '下级列表', icon: 'list', noCache: true, parent_module_id: 0, module_id: 1, activeMenu: '/stores/0/module_id/1', roles: [1] },
       hidden: true
     },
     {
@@ -36,6 +36,13 @@ export default {
       component: () => import('@/views/store-images/index'),
       name: '品牌形象',
       meta: { title: '品牌形象', noCache: true, activeMenu: '/stores/0/module_id/1', roles: [1] },
+      hidden: true
+    },
+    {
+      path: 'module_id/:parent_id(\\d+)/incomes/:store_id(\\d+)',
+      component: () => import('@/views/store-incomes/index'),
+      name: '运营状况',
+      meta: { title: '运营状况', noCache: true, activeMenu: '/stores/0/module_id/1', roles: [1] },
       hidden: true
     }
   ]
